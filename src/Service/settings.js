232c0c86863e20f86bf490d2e4ec97106980a63f
yusuftekinx <79636811+yusuftekinx@users.noreply.github.com>
2021-11-09 13:05:54 +0300

    Update settings.js

--- src/Service/settings.js
+++ src/Service/settings.js
@@ -1,3 +1,3 @@
-export const BASE_URL = 'https://teneke-kafalar-api.herokuapp.com//api/v2'
-export const imageApiUrl = "https://teneke-kafalar-api.herokuapp.com//public/CommunityProfile/";
+export const BASE_URL = 'https://teneke-kafalar-api.herokuapp.com/api/v2'
+export const imageApiUrl = "https://teneke-kafalar-api.herokuapp.com/public/CommunityProfile/";
 export const API_HOST = 'https://teneke-kafalar-api.herokuapp.com/'
